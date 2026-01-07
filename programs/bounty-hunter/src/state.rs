405e928c24c9cd127156b6064d073280524cbcf1
rute-sophie <rute.c.lemos@gmail.com>
2026-01-07 00:32:42 +0000

    state added

---
+++ programs/bounty-hunter/src/state.rs
@@ -0,0 +1,25 @@
+use anchor_lang::prelude::*;
+
+#[derive(InitSpace)]
+#[account(discriminator = 1)]
+pub struct Bounty {
+    pub seed: u64,
+    #[max_len(1024)]
+    pub description: String,
+    #[max_len(100)]
+    pub link: String,
+    pub reward: u64,
+    pub bump: u8,
+    pub authority: Pubkey,
+}
+
+#[derive(InitSpace)]
+#[account(discriminator = 1)]
+pub struct Submission {
+    pub bounty: Pubkey,
+    #[max_len(100)]
+    pub link: String,
+    pub hunter: Pubkey,
+    #[max_len(1024)]
+    pub notes: String,
+}
