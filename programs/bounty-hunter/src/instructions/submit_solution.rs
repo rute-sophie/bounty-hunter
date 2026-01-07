66068c4b1102a72c470ea9e465c7b115849f1508
rute-sophie <rute.c.lemos@gmail.com>
2026-01-07 22:36:49 +0000

    implemented ctx of submit solution

--- programs/bounty-hunter/src/instructions/submit_solution.rs
+++ programs/bounty-hunter/src/instructions/submit_solution.rs
@@ -1,9 +1,24 @@
+use crate::{Bounty, Submission};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
-pub struct SubmitSolution {}
+pub struct SubmitSolution<'info> {
+    #[account(mut)]
+    pub hunter: Signer<'info>,
+    #[account()]
+    pub bounty: Account<'info, Bounty>,
+    #[account(
+        init_if_needed,
+        payer = hunter,
+        space = Submission::INIT_SPACE + Submission::DISCRIMINATOR.len(),
+        seeds = [b"submission", hunter.key().as_ref(), bounty.key().as_ref()],
+        bump,
+    )]
+    pub submission: Account<'info, Submission>,
+
+    pub system_program: Program<'info, System>,
+}
 
 pub fn handler(ctx: Context<SubmitSolution>) -> Result<()> {
-    
     Ok(())
 }
