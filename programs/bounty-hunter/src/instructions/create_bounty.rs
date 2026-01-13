2bcd91867e4984a78c5aa89a2ae9c15112185c40
rute-sophie <rute.c.lemos@gmail.com>
2026-01-13 00:03:22 +0000

    token interface accounts added

--- programs/bounty-hunter/src/instructions/create_bounty.rs
+++ programs/bounty-hunter/src/instructions/create_bounty.rs
@@ -2,6 +2,11 @@ use anchor_lang::prelude::*;
 
 use crate::state::Bounty;
 
+//use anchor_spl::token::{Token};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::TokenInterface};
+use anchor_spl::token_interface::Mint;
+use anchor_spl::token_interface::TokenAccount;
+
 #[derive(Accounts)]
 #[instruction(seed: u64)]
 pub struct CreateBounty<'info> {
@@ -17,7 +22,32 @@ pub struct CreateBounty<'info> {
     )]
     pub bounty: Account<'info, Bounty>,
 
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    // the token account associated with the maker and mint used to deposit tokens in the vault
+    #[account(
+            mut,
+            associated_token::mint = mint,
+            associated_token::authority = bounty,
+            associated_token::token_program = token_program,
+        )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    // the token account associated with the escrow and mint where deposit tokens are parked
+    #[account(
+            init,
+            payer = maker,
+            associated_token::mint = mint,
+            associated_token::authority = bounty,
+            associated_token::token_program = token_program
+        )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
     pub system_program: Program<'info, System>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>
 }
 
 impl CreateBounty<'_> {
