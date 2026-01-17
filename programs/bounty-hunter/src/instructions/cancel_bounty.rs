b81d6ce5b396464b3d6bcb22df6dd5d857dc1c20
rute-sophie <rute.c.lemos@gmail.com>
2026-01-17 16:38:49 +0000

    cancel bounty : refunding tokens

--- programs/bounty-hunter/src/instructions/cancel_bounty.rs
+++ programs/bounty-hunter/src/instructions/cancel_bounty.rs
@@ -1,6 +1,10 @@
 use anchor_lang::prelude::*;
 
 use crate::{error::BountyHunterErrors, state::Bounty};
+use anchor_spl::token::{close_account, transfer_checked, CloseAccount, TransferChecked};
+use anchor_spl::token_interface::Mint;
+use anchor_spl::token_interface::TokenAccount;
+use anchor_spl::{associated_token::AssociatedToken, token_interface::TokenInterface};
 
 #[derive(Accounts)]
 pub struct CancelBounty<'info> {
@@ -14,11 +18,69 @@ pub struct CancelBounty<'info> {
     )]
     pub bounty: Account<'info, Bounty>,
 
-    pub system_program: Program<'info, System>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = bounty,
+        associated_token::token_program = token_program,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program,
+    )]
+    pub maker_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 impl CancelBounty<'_> {
-    pub fn handler(_ctx: Context<CancelBounty>) -> Result<()> {
+    pub fn handler(ctx: Context<CancelBounty>) -> Result<()> {
+        ctx.accounts.refund_tokens()?;
+        Ok(())
+    }
+
+    fn refund_tokens(&self) -> Result<()> {
+        let bounty_seeds = [
+            b"bounty",
+            self.maker.key.as_ref(),
+            &self.bounty.seed.to_le_bytes(),
+            &[self.bounty.bump],
+        ];
+        let signer_seeds = [bounty_seeds.as_ref()];
+
+        transfer_checked(
+            CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                TransferChecked {
+                    from: self.vault.to_account_info(),
+                    mint: self.mint.to_account_info(),
+                    to: self.maker_token_account.to_account_info(),
+                    authority: self.bounty.to_account_info(),
+                },
+                signer_seeds.as_ref(),
+            ),
+            self.vault.amount,
+            self.mint.decimals,
+        )?;
+
+        close_account(CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            CloseAccount {
+                account: self.vault.to_account_info(),
+                destination: self.maker.to_account_info(),
+                authority: self.bounty.to_account_info(),
+            },
+            signer_seeds.as_ref(),
+        ))?;
+
         Ok(())
     }
 }
